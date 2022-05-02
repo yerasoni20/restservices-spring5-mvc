2bd73f92ecdca94deed8ee510ec39de93f8c0325
yerasoni20 <32177481+yerasoni20@users.noreply.github.com>
2022-05-02 16:48:48 -0400

    Creating Vendor and repository

--- src/main/java/guru/springframework/spring5webfluxrest/repositories/VendorRepository.java
+++ src/main/java/guru/springframework/spring5webfluxrest/repositories/VendorRepository.java
@@ -3,8 +3,5 @@ package guru.springframework.spring5webfluxrest.repositories;
 import guru.springframework.spring5webfluxrest.domain.Vendor;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
-/**
- * Created by jt on 12/23/17.
- */
 public interface VendorRepository extends ReactiveMongoRepository<Vendor, String> {
 }
